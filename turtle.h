522c78cd7625156ce0d693de6e6729cc46550e16
levyxx <128173120+levyxx@users.noreply.github.com>
2024-10-22 20:54:55 +0900

    Add files via upload

---
+++ turtle.h
@@ -0,0 +1,11 @@
+void set_angle(double r);
+void set_pos(int x, int y);
+void set_dpos(double x, double y);
+void dforward(double len, GC gc);
+void dturn(double r);
+void turn(int r);
+double get_posx();
+double get_posy();
+double get_angle();
+void up();
+void down();
